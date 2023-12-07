dd01ea8e4b2b810d07c55c8ab4e34a8844f32b87
meotrang3051 <dinhhieu3051@gmail.com>
2023-12-07 23:52:59 +0700

    làm topbar và lỗi tùm lum

---
+++ solokiil.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/css.css">
+    <script src="../vendor/jquery-3.6.0.min.js"></script>
+    <script src="../vendor/popper.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../vendor/bootstrap/js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- mai hỏi thầy tại sao lại mất cái img -->
+        <!-- header -->
+        <div class="header">
+            <!-- topbar -->
+            <div class="topbar">
+                <img src="./img/quang_caotop-1.png" alt="">
+
+                <div class="topbar-content">
+                <div class="logo">
+                    <img src="./img/ads4.jpg" alt="">
+                </div>
+                <div class="gioithieu"></div>
+                <div class="lohin"></div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
+</body>
+
+</html>
