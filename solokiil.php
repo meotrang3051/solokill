de7d3014987901850f1949c06b02854f6e3bc924
meotrang3051 <dinhhieu3051@gmail.com>
2023-12-19 01:50:57 +0700

    đang làm cái navbar

--- solokiil.php
+++ solokiil.php
@@ -7,13 +7,16 @@
     <title>Document</title>
     <link rel="stylesheet" href="./css/css.css">
     <script src="../vendor/jquery-3.6.0.min.js"></script>
-    <script src="../vendor/popper.min.js"></script>
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="../vendor/bootstrap/js/bootstrap.min.js">
+    <script src="../vendor/popper.min.js"></script>  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    
 </head>
 
 <body>
@@ -23,17 +26,74 @@
         <div class="header container-fluid">
             <!-- topbar -->
             <div class="topbar">
-                <div class="topbar-img">
+                <div class="topbar-img d-flex justify-content-center">
                 </div>
-            </div>
+                <div class="topbar-content container">
+                    <div class="row container">
+                        <div class="logo col-3 text-left">
+                            <a href="#"><img src="./img/lg.png" alt=""></a>
+                        </div>
+                        <div class="gioithieu col-6 text-center mt-2">
+                            <h3>TỰ TIN GIAO TIẾP VỚI ENGLISH</h3>
+                        </div>
+                        <div class="login col-3 text-right mt-2">
+                            <button type="button" class="btn btn-dark">Đăng Ký</button>
+                            <button type="button" class="btn btn-outline-primary">Đăng Nhập</button>
+                        </div>
+                    </div>
 
-            <div class="topbar-content">
-                <div class="logo">
-                    <img src="./img/lg.png" alt="">
                 </div>
-                <div class="gioithieu"></div>
-                <div class="lohin"></div>
             </div>
+            <div class="navbar">
+                <div class=" container">
+                    <div class="row container-fluid   ">
+                        <a class="nav-link col-1.3" href="#">Trang Chủ</a>
+
+                        <a class="nav-link dropdown-toggle col-1.3" data-toggle="dropdown" href="#">Về chúng tôi</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Báo Chí Nói Về Chúng Tôi</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+
+                        <a class="nav-link dropdown-toggle col-1.3" data-toggle="dropdown" href="#">Học Viên </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Báo Chí Nói Về Chúng Tôi</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+
+                        <a class="nav-link dropdown-toggle col-1.3" data-toggle="dropdown" href="#">Lịch Khai Giảng</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Báo Chí Nói Về Chúng Tôi</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+
+                        <a class="nav-link dropdown-toggle col-1.3" data-toggle="dropdown" href="#">Khóa Học </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Báo Chí Nói Về Chúng Tôi</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+
+                        <a class="nav-link dropdown-toggle col-1.3" data-toggle="dropdown" href="#">test review</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Báo Chí Nói Về Chúng Tôi</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+
+                        <a class="nav-link col-1.3" href="#"> Tin Tức</a>
+
+                        <a class="nav-link col-1.3" href="#">Liên Hệ </a>
+
+                       <!-- mai tai lại thành từng div ko dung row -->
+                    </div>
+                </div>
+            </div>
+          
+
 
 
         </div>
